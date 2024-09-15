6fe286221ee7edb4162fc5a3b9ffd323b1ce4b98
rmmsia <rmmsia@gmail.com>
2024-09-15 15:30:27 +0800

    functioning web app

--- generate.py
+++ generate.py
@@ -1,28 +1,16 @@
-from datetime import datetime
 from grouping import GreedyGroupManager
-import file_input_util as fiu
 
-def generate():
-    # User input to pass in pairing scores csv
-    pairing_scores_file = fiu.pairing_score_file()
+def generate(pairing_scores_file: str, attendees_file: str, group_size: int):
 
     # Create instance of GreedyGroupManager class
     manager = GreedyGroupManager(pairing_scores_file=pairing_scores_file)
 
-    # Pass attendees txt file - get list of attendees for session
-    attendees = fiu.attendees_file()
-
-    # Set group size
-    group_size = int(input("Enter the group size: "))
+    # Parse attendees
+    with open(attendees_file, 'r') as file:
+            attendees = [line.strip() for line in file]
 
     # Create groups
     print()
     groups = manager.create_groups(group_size=group_size, attendees=attendees)
-    for i, group in enumerate(groups, 1):
-        print(f"Group {i}: {group}")
-
-    # Save updated pairing scores
-    timestamp = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-    manager.save_pairing_scores(f"pairing_scores_{timestamp}.csv")
-    print()
-    print(f"Pairing scores saved to 'pairing_scores_{timestamp}.csv'")
+    
+    return groups
