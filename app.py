efaaebb9adc32e6b212f2e7f803481dad0b61f58
rmmsia <rmmsia@gmail.com>
2024-09-18 15:00:29 +0800

    automatic deletion of upload
    for data security

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify, send_from_directory
-import os
 from generate import generate
+import os
+import shutil
 
 app = Flask(__name__)
 
@@ -8,6 +9,18 @@ app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads'
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
+
+
+def clear_upload_folder():
+    for filename in os.listdir(UPLOAD_FOLDER):
+        file_path = os.path.join(UPLOAD_FOLDER, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print(f'Failed to delete {file_path}. Reason: {e}')
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -44,6 +57,8 @@ def index():
             except Exception as e:
                 print(f"Error: {e}")
                 return jsonify({'error': str(e)}), 500
+            finally:
+                clear_upload_folder()
 
     return render_template('index.html')
 
