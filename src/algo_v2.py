b183eab77317c9f6337e11fd62aa1470bfb34aff
rmmsia <rmmsia@gmail.com>
2025-01-16 14:43:06 +0800

    sunsetted old code; algo-v2 rollout

--- src/algo_v2.py
+++ src/algo_v2.py
@@ -3,8 +3,9 @@ import pandas as pd
 from collections import defaultdict
 
 # Global
-list_shuffle_seed = random.seed(694519) # for shuffling attendees (remove bias towards initial list order)
-selector_seed = random.Random(42) # for tiebreaker
+list_shuffle_seed = random.seed(694519)  # for shuffling attendees (remove bias towards initial list order)
+selector_seed = random.Random(42)  # for tiebreaker
+
 
 class Attendee:
     def __init__(self, name, gender, telegram_id, email, year, faculty):
@@ -14,9 +15,9 @@ class Attendee:
         self.email = email
         self.year = year
         self.faculty = faculty
-    
+
     def __repr__(self):
-        return f'{self.name} ({self.telegram_id}, {self.gender}, {self.year}, {self.faculty})'
+        return f'{self.name} ({self.email}, {self.telegram_id}, {self.gender}, {self.year}, {self.faculty})'
 
 
 def assign_groups(attendees, group_size, matrix, weights):
@@ -37,6 +38,7 @@ def assign_groups(attendees, group_size, matrix, weights):
                 diversity_score = calc_diversity_score(candidate, group, weights)
 
                 # Calculate repetition penalty
+                # TODO: Use email in future as that is immutable, for now use telegram_id
                 repeat_penalty = sum(
                     matrix.loc[candidate.telegram_id, member.telegram_id] for member in group
                 )
@@ -50,7 +52,7 @@ def assign_groups(attendees, group_size, matrix, weights):
                 # Probabilistic tiebreaker
                 elif score == best_score and selector_seed.random() < 0.5:
                     best_candidate = candidate
-            
+
             if best_candidate:
                 group.append(best_candidate)
                 ungrouped.remove(best_candidate)
@@ -70,7 +72,7 @@ def calc_diversity_score(candidate, group, weights):
         gender_count[member.gender] += 1
         year_count[member.year] += 1
         faculty_count[member.faculty] += 1
-    
+
     # Get candidate's attributes
     gender = candidate.gender
     year = candidate.year
@@ -83,16 +85,19 @@ def calc_diversity_score(candidate, group, weights):
 
     return gender_score + year_score + faculty_score
 
+
 def load_pairing_score_matrix(file_path):
     return pd.read_csv(file_path, index_col=0)
+
 
 def save_pairing_score_matrix(matrix, file_path):
     matrix.to_csv(file_path)
 
+
 def update_pairing_scores(groups, matrix):
     for group in groups:
         for i, member1 in enumerate(group):
-            for member2 in group[i+1:]:
+            for member2 in group[i + 1:]:
                 # Update the pairing scores matrix
                 matrix.loc[member1.name, member2.name] += 1
                 matrix.loc[member2.name, member1.name] += 1
@@ -110,8 +115,10 @@ Returns:
 - groups (List[List[Attendee]]): list of groups, where each group is a list of Attendee objects
 '''
 
+
 def main_workflow(pairing_scores_csv, attendees_csv, weights_list, group_size):
     # Load attendees
+    print("Loading attendees")
     attendees_df = pd.read_csv(attendees_csv)
     attendees_df.set_index('Name', drop=False, inplace=True)
 
@@ -119,7 +126,7 @@ def main_workflow(pairing_scores_csv, attendees_csv, weights_list, group_size):
         row['Name']: Attendee(row['Name'], row['Gender'], row['Telegram ID'], row['Email'], row['Year'], row['Faculty'])
         for _, row in attendees_df.iterrows()
     }
-    attendees = {attendee.name: attendee for attendee in attendees.values()} # key is name, value is Attendee object
+    attendees = {attendee.name: attendee for attendee in attendees.values()}  # key is name, value is Attendee object
 
     # Load historical pairing scores
     try:
@@ -127,7 +134,7 @@ def main_workflow(pairing_scores_csv, attendees_csv, weights_list, group_size):
     except FileNotFoundError:
         attendee_names = list(attendees.keys())
         pairing_scores = pd.DataFrame(0, index=attendee_names, columns=attendee_names)
-    
+
     weights = {
         'gender': weights_list[0],
         'year': weights_list[1],
@@ -136,25 +143,24 @@ def main_workflow(pairing_scores_csv, attendees_csv, weights_list, group_size):
 
     groups = assign_groups(attendees, group_size, pairing_scores, weights)
 
+    print("Successfully generated groups")
     return groups
 
-'''
-print_groups - print the groups and pairing scores within each group
-Input:
-- groups (List[List[Attendee]]): list of groups, where each group is a list of Attendee objects
-- pairing_scores (pd.DataFrame): DataFrame containing pairing scores between attendees
-'''
 
-def print_groups(groups, pairing_scores):
-    for i, group in enumerate(groups, start=1):
-        members = [member.name for member in group]
-        print(f'Group {i}: {', '.join(members)}')
-    # print pairing scores within the group
-    j = 0
-    for group in groups:
-        print(f'Group {j+1}')
-        j += 1
-        for i, member1 in enumerate(group):
-            for member2 in group[i+1:]:
-                print(f'{member1.name} - {member2.name}: {pairing_scores.loc[member1.telegram_id, member2.telegram_id]}')
-        print()
+def groups_to_dataframe(groups):
+    '''
+    Input: groups (List[List[Attendee]]): list of groups, where each group is a list of Attendee objects
+    Output: df (pd.DataFrame): DataFrame containing attendees and all attribtes, but now with a 'Group' column
+    '''
+    data = []
+
+    for idx, group in enumerate(groups, start=1):
+        for member in group:
+            member_data = vars(member)
+            member_data['Group'] = idx
+            data.append(member_data)
+
+    df = pd.DataFrame(data)
+    df.set_index('name', inplace=True)
+
+    return df
